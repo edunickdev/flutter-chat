6dbdc70540d15b90be5ab894dfd16a1669d3ec21
El Código Recuerda <114629663+edunickdev@users.noreply.github.com>
2023-04-02 14:01:35 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:firebase_chat/screens/chat_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Chat',
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
+      home: ChatScreen(),
+    );
+  }
+}
+
+
